036ec427a7ef18743c1e042d69e98260f0651054
Miidoriya <99621562+Miidoriya@users.noreply.github.com>
2023-02-13 19:32:51 +0000

    [working-with-html] Tests updated and new subscription route added

--- src/lib.rs
+++ src/lib.rs
@@ -6,9 +6,20 @@ async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     println!("Endpoint: {}", listener.local_addr().unwrap().port());
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check))
+        .route("/subscriptions", web::post().to(subscribe)))
         .listen(listener)?
         .run();
     Ok(server)
